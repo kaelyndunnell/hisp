c74cbacd91e7abfb1038efe64475671897b9f4f2
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-10-29 09:56:13 -0400

    moved example to CI

--- test/test_scenario.py
+++ test/test_scenario.py
@@ -1,6 +1,8 @@
 from hisp.helpers import Scenario
 import os
 import pytest
+import numpy as np
+import matplotlib.pyplot as plt
 
 current_dir = os.path.dirname(__file__)
 scenario_path = os.path.join(current_dir, "scenario_test.txt")
@@ -25,3 +27,29 @@ def test_get_pulse_row(t, expected_row):
     pulse_row = my_scenario.get_row(t=t)
 
     assert pulse_row == expected_row
+
+
+def test_reading_a_file():
+    my_scenario = Scenario(scenario_path)
+
+    times = np.linspace(0, my_scenario.get_maximum_time(), 1000)
+    pulse_types = []
+    for t in times:
+        pulse_type = my_scenario.get_pulse_type(t)
+        pulse_types.append(pulse_type)
+
+    # color the line based on the pulse type
+    color = {
+        "FP": "red",
+        "ICWC": "blue",
+        "RISP": "green",
+        "GDC": "orange",
+        "BAKE": "purple",
+    }
+
+    colors = [color[pulse_type] for pulse_type in pulse_types]
+
+    for i in range(len(times) - 1):
+        plt.plot(times[i : i + 2], np.ones_like(times[i : i + 2]), c=colors[i])
+    # plt.xscale("log")
+    # plt.show()
