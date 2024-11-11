6b45aee03550cfd63cb9b1b9a189e395b0b513ed
kaelyndunnell <kaelyndunnell03@gmail.com>
2024-11-10 19:23:45 -0600

    added B mb and DFW mb tests

--- test/test_mb_model_festim.py
+++ test/test_mb_model_festim.py
@@ -1,4 +1,4 @@
-from hisp.festim_models.mb_model import make_W_mb_model
+from hisp.festim_models.mb_model import make_W_mb_model, make_B_mb_model, make_DFW_mb_model
 
 import festim as F
 
@@ -7,8 +7,55 @@ import pytest
 
 @pytest.mark.parametrize("temp", [400, lambda t: 400 + 1, lambda x, t: 400 + t - x[0]])
 def test_mb_model(temp):
-    """Builds a festim model, run it, and tests the output."""
+    """Builds a festim tungsten model, run it, and tests the output."""
     (mb_model, quantities) = make_W_mb_model(
+        temperature=temp,
+        deuterium_ion_flux=lambda _: 1e22,
+        deuterium_atom_flux=lambda _: 1e22,
+        tritium_ion_flux=lambda _: 1e22,
+        tritium_atom_flux=lambda _: 1e22,
+        final_time=50,
+        folder=".",
+    )
+
+    mb_model.initialise()
+    mb_model.run()
+
+    # TEST
+    assert isinstance(quantities, dict)
+    for key, value in quantities.items():
+        assert isinstance(key, str)
+        assert isinstance(value, F.TotalVolume)
+        assert len(value.data) > 0
+
+
+@pytest.mark.parametrize("temp", [400, lambda t: 400 + 1, lambda x, t: 400 + t - x[0]])
+def test_mb_model(temp):
+    """Builds a festim boron model, run it, and tests the output."""
+    (mb_model, quantities) = make_B_mb_model(
+        temperature=temp,
+        deuterium_ion_flux=lambda _: 1e22,
+        deuterium_atom_flux=lambda _: 1e22,
+        tritium_ion_flux=lambda _: 1e22,
+        tritium_atom_flux=lambda _: 1e22,
+        final_time=50,
+        folder=".",
+    )
+
+    mb_model.initialise()
+    mb_model.run()
+
+    # TEST
+    assert isinstance(quantities, dict)
+    for key, value in quantities.items():
+        assert isinstance(key, str)
+        assert isinstance(value, F.TotalVolume)
+        assert len(value.data) > 0
+
+@pytest.mark.parametrize("temp", [400, lambda t: 400 + 1, lambda x, t: 400 + t - x[0]])
+def test_mb_model(temp):
+    """Builds a festim tungsten model, run it, and tests the output."""
+    (mb_model, quantities) = make_DFW_mb_model(
         temperature=temp,
         deuterium_ion_flux=lambda _: 1e22,
         deuterium_atom_flux=lambda _: 1e22,
