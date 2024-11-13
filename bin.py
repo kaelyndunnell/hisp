26f07ac7d0c3489309e6c5db402b05e187f6d6f1
kaelyndunnell <kaelyndunnell03@gmail.com>
2024-11-13 11:27:54 -0500

    update bin class structure

--- bin.py
+++ bin.py
@@ -1,11 +1,15 @@
-class BinCollection:
-    pass
-
-class Reactor:
-    first_wall: BinCollection
-    divertor: BinCollection
+from typing import List
 
 class Bin:
+    index: int
+    sub_bins: list
+    length: float
+    material: str
+    shadowed: bool
+    low_wetted: bool
+    high_wetted: bool
+    dfw: bool
+
     def __init__(
         self,
         index: int,
@@ -26,4 +30,19 @@ class Bin:
         self.high_wetted = high_wetted
         self.dfw = dfw
 
-    
+class BinCollection:
+    def __init__(self, bins: List[Bin] = None):
+        """Initializes a BinCollection object containing several bins.
+
+        Args:
+            bins: The list of bins in the collection. Each bin is a Bin object.
+        """
+        self._bins = bins if bins is not None else []
+
+    @property
+    def bins(self) -> List[Bin]:
+        return self._bins
+
+class Reactor:
+    first_wall: BinCollection
+    divertor: BinCollection
