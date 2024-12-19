04ad1aa077be6a28565eec82f686f0db00983143
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-12-19 17:43:39 -0500

    added a test to catch the bug

---
+++ test/test_helpers.py
@@ -0,0 +1,12 @@
+from hisp.helpers import gaussian_distribution
+
+import numpy as np
+
+
+def test_gaussian_distr_area():
+    x = np.linspace(-10, 10, 1000)
+    y = gaussian_distribution([x], mean=0, width=1, mod=np)
+    area = np.trapz(y, x)
+    assert np.isclose(
+        area, 1, atol=1e-2
+    ), f"Area under Gaussian distribution is {area} instead of 1"
