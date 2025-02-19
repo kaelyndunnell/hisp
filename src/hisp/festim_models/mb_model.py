503e2571ce1a7b6a3263e5c593704666f9636607
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-02-19 10:02:19 -0500

    adjust dfw rtol

--- src/hisp/festim_models/mb_model.py
+++ src/hisp/festim_models/mb_model.py
@@ -779,7 +779,7 @@ def make_DFW_mb_model(
     ############# Settings #############
     my_model.settings = F.Settings(
         atol=1e10,
-        rtol=1e-10,
+        rtol=1e-8,
         max_iterations=30,
         final_time=final_time,
     )
